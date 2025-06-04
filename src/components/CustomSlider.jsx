42067f9468b0c690785ae19d88f3c05f56493bc9
Nupur Bishnoi <nupurbishnoi03@gmail.com>
2025-06-04 11:07:47 +0530

    keyframe done

--- src/components/CustomSlider.jsx
+++ src/components/CustomSlider.jsx
@@ -4,30 +4,25 @@ import { SLIDER_LEFT_IMAGES_LIST, SLIDER_RIGHT_IMAGES_LIST } from "../utils/help
 const CustomSlider = () => {
     return (
         <div id="overview" className="overflow-hidden mx-auto max-w-[1920px]">
-            <div className="overflow-hidden w-full">
-                <div className="flex slider-left w-max">
-                    {[...SLIDER_LEFT_IMAGES_LIST, ...SLIDER_LEFT_IMAGES_LIST].map((obj, index) => (
-                        <img
-                            key={index}
-                            src={obj}
-                            alt={`slider-left-${index}`}
-                            className="pointer-events-none w-[320px] lg:h-[320px] h-[200px] object-cover"
-                        />
-                    ))}
-                </div>
+            <div className="flex slider-left">
+                {SLIDER_LEFT_IMAGES_LIST.map((obj, index) => (
+                    <img
+                        key={index}
+                        src={obj}
+                        alt={`slider-left-${index + 1}`}
+                        className="pointer-events-none w-[320px] lg:h-[320px] h-[200px] object-cover"
+                    />
+                ))}
             </div>
-
-            <div className="overflow-hidden w-full">
-                <div className="flex slider-right w-max">
-                    {[...SLIDER_RIGHT_IMAGES_LIST, ...SLIDER_RIGHT_IMAGES_LIST].map((obj, index) => (
-                        <img
-                            key={index}
-                            src={obj}
-                            alt={`slider-right-${index}`}
-                            className="pointer-events-none w-[320px] lg:h-[320px] h-[200px] object-cover"
-                        />
-                    ))}
-                </div>
+            <div className="flex slider-right">
+                {SLIDER_RIGHT_IMAGES_LIST.map((obj, index) => (
+                    <img
+                        key={index}
+                        src={obj}
+                        alt={`slider-right-${index + 1}`}
+                        className="pointer-events-none w-[320px] lg:h-[320px] h-[200px] object-cover"
+                    />
+                ))}
             </div>
         </div>
     );
